2d076acbcd37cb5246137aa70621a8dfd14ef396
wilhelium <11941973+wilhelium@users.noreply.github.com>
2023-10-31 21:57:08 +1300

    Add View to retrieve Markdown content
    Why:
    
      - No View exists that will retrieve the content of a Wiki page and display it to the user.
    
    Outstanding Task:
    
      - The URL pattern is Case sensitive and must match the markdown file
      exactly.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki_entry(request, page_title):
+    return render(request, "encyclopedia/wikiPage.html", {
+        "markdown_content": util.get_entry(page_title)
+    })
